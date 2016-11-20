d69e58b1b90a07e61ad19f5af1524d851ff0348a
Naveen Kumar Gurram <gurram.naveen@gmail.com>
2016-11-20 01:43:19 -0500

    Option Page updates

---
+++ js/isitup.js
@@ -0,0 +1,292 @@
+$(document).ready(function () {
+
+  var notApplicable = "N/A";
+  var gifLoadingEle = "<img src='./img/ajax-loader.gif'>";
+  var healthResp = "Only Json is displayed";
+
+  var allowIndividualRetry = localStorage["allowIndividualRetry"];
+  var allowAutomaticRefresh=localStorage["allowAutomaticRefresh"];
+  var pushNotifications=localStorage["pushNotifications"];
+  var pageRefreshAfter=localStorage["pageRefreshAfter"];
+  var cfgTxt=localStorage["cfgTxt"];
+
+
+  var configuration = [
+    {
+      "name": "Index",
+      "envs": [
+        {
+          "name": "dev",
+          "url": ""
+        },
+        {
+          "name": "int",
+          "url": ""
+        },
+        {
+          "name": "int3",
+          "url": ""
+        },
+        {
+          "name": "qa",
+          "url": ""
+        },
+        {
+          "name": "qa-uat",
+          "url": ""
+        },
+        {
+          "name": "qa-pm",
+          "url": ""
+        },
+        {
+          "name": "prod",
+          "url": ""
+        }
+      ]
+    },
+    {
+      "name": "Cabinet",
+      "envs": [
+        {
+          "name": "dev",
+          "url": ""
+        },
+        {
+          "name": "int",
+          "url": ""
+        },
+        {
+          "name": "int3",
+          "url": ""
+        },
+        {
+          "name": "qa",
+          "url": ""
+        },
+        {
+          "name": "qa-uat",
+          "url": ""
+        },
+        {
+          "name": "qa-pm",
+          "url": ""
+        },
+        {
+          "name": "prod",
+          "url": ""
+        }
+      ]
+    },
+    {
+      "name": "Dyno",
+      "envs": [
+        {
+          "name": "dev",
+          "url": ""
+        },
+        {
+          "name": "int",
+          "url": ""
+        },
+        {
+          "name": "int3",
+          "url": ""
+        },
+        {
+          "name": "qa",
+          "url": ""
+        },
+        {
+          "name": "qa-uat",
+          "url": ""
+        },
+        {
+          "name": "qa-pm",
+          "url": ""
+        },
+        {
+          "name": "prod",
+          "url": ""
+        }
+      ]
+    },
+    {
+      "name": "ERM",
+      "envs": [
+        {
+          "name": "dev",
+          "url": ""
+        },
+        {
+          "name": "int",
+          "url": ""
+        },
+        {
+          "name": "int3",
+          "url": ""
+        },
+        {
+          "name": "qa",
+          "url": ""
+        },
+        {
+          "name": "qa-uat",
+          "url": ""
+        },
+        {
+          "name": "qa-pm",
+          "url": ""
+        },
+        {
+          "name": "prod",
+          "url": "http://google.com"
+        }
+      ]
+    }
+  ];
+  var componentMap = {};
+  // create a componentMap 
+  for (i = 0; i < configuration.length; i++) {
+    componentMap[configuration[i].name] = configuration[i].envs;
+  }
+  var environments = [];
+  var component = componentMap[configuration[0].name];
+  // create array of elements
+  for (var i = 0; i < component.length; i++) {
+    environments.push(component[i].name);
+  }
+
+  var isitupHtml = "<table id='envStatusTbl' class='table table-bordered table-striped table-hover table-condensed'>";
+  isitupHtml += "<thead><tr class='info'><th></th>";
+  for (var i = 0; i < environments.length; i++) {
+    isitupHtml += ("<th>" + environments[i].toUpperCase() + "</th>");
+  }
+  isitupHtml += "</tr></thead><tbody>";
+  var spanIdMap = {};
+
+  $.each(componentMap, function (key, value) {
+    isitupHtml += ("<tr><td><b>" + key + "</b></td>");
+    for (var i = 0; i < value.length; i++) {
+      spanId = (key + "-" + value[i].name);
+      if (value[i].url) {
+        isitupHtml += ("<td><span id='" + spanId + "'>" + gifLoadingEle + "</span>");
+        // if individual retry is allowed.
+        if(allowIndividualRetry){
+        isitupHtml += (" <span id='refreshId' data='" + spanId + "' class='status-code refresh glyphicon glyphicon-refresh'></span>");
+        }
+        isitupHtml += "</td>";
+        spanIdMap[spanId] = value[i].url;
+      } else {
+        isitupHtml += ("<td><span class='status-code na'>" + notApplicable + "</span></td>");
+      }
+
+    }
+    isitupHtml += "</tr>"
+  });
+  isitupHtml += "</tbody></table>";
+  // now update table 
+  $('#isitupId').html(isitupHtml);
+  // now check health
+  checkHealth(spanIdMap);
+  // allow automatic refresh
+  if(allowAutomaticRefresh){
+    // refresh status every x seconds
+    window.setInterval(function(){
+      /// call your function here
+      checkHealth(spanIdMap);
+    }, parseInt(pageRefreshAfter)); 
+  }
+
+  $("span").click(function () {
+    var spanData = $(this).attr("data");
+    if ($(this).text() === notApplicable) {
+      $('#status-detail').html("");
+      $('#status-detail').hide();
+      $('#health').hide();
+      return;
+    }
+    // when refresh is clicked, just re-run the health
+    if ($(this).hasClass("glyphicon-refresh")) {
+      var healthMap = {};
+      healthMap[spanData] = spanIdMap[spanData];
+      $('#' + spanData).removeClass("status-code success");
+      $('#' + spanData).html(gifLoadingEle);
+      checkHealth(healthMap);
+      return;
+    }
+    var IS_JSON = true;
+    try {
+      var json = $.parseJSON(spanData);
+    }
+    catch (err) {
+      IS_JSON = false;
+    }
+    if (!IS_JSON) {
+      spanData = "Not JSON";
+    }
+    $('#status-detail').html(spanData);
+    $('#status-detail').show();
+    $('#health').show();
+  });
+
+});
+/**
+ * Check health of each url.
+ */
+function checkHealth(spanIdMap) {
+  var notificationMessage="";
+  // now fire all url's and callback will update span with corresponding style and the content.
+  $.each(spanIdMap, function (key, value) {
+    $.ajax({
+      url: value,
+      type: 'GET',
+      success: function (data) {
+        data.url = value;
+        $('#' + key).addClass("status-code success");
+        $('#' + key).attr('data', data);
+        $('#' + key).text("200");
+      },
+      error: function (jqXHR, error, errorThrown) {
+        notificationMessage += (key + " = " + value + "\n");
+        if (jqXHR.status) {
+          var statusData = {};
+          statusData.error = errorThrown;
+          statusData.url = value;
+          $('#' + key).addClass("status-code error");
+          $('#' + key).attr('data', statusData);
+          $('#' + key).text(jqXHR.status);
+        } else {
+          var statusData = {};
+          statusData.error = 'Failed to send xmlhttprequest';
+          statusData.url = value;
+          $('#' + key).addClass("status-code error");
+          $('#' + key).attr('data', statusData);
+          $('#' + key).text(jqXHR.status);
+        }
+        sendNotification(notificationMessage);
+      },
+      complete: function () {
+        $('#' + key).removeClass("loading");
+      }
+    });
+  });
+
+  /**
+   * Send Notification
+   */
+  function sendNotification(notificationMessage)
+  {
+    // if enabled
+     if(pushNotifications){           
+       var options = {
+         type: "basic",
+         title: "Environment is Down",
+         iconUrl: 'img/isitup16.png',
+         message: (notificationMessage)
+       }
+       chrome.notifications.create("", options, function (cb) {
+       });
+    }
+  }
+};
--- js/options.js
+++ js/options.js
@@ -1,32 +1,74 @@
-// Saves options to chrome.storage.sync.
-function save_options() {
-    var color = document.getElementById('color').value;
-    var likesColor = document.getElementById('like').checked;
-    chrome.storage.sync.set({
-        favoriteColor: color,
-        likesColor: likesColor
-    }, function() {
-        // Update status to let user know options were saved.
-        var status = document.getElementById('status');
-        status.textContent = 'Options saved.';
-        setTimeout(function() {
-            status.textContent = '';
-        }, 750);
-    });
-}
+$(document).ready(function () {
 
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
-function restore_options() {
+
     // Use default value color = 'red' and likesColor = true.
     chrome.storage.sync.get({
-        favoriteColor: 'red',
-        likesColor: true
-    }, function(items) {
-        document.getElementById('color').value = items.favoriteColor;
-        document.getElementById('like').checked = items.likesColor;
+        allowIndividualRetry: false,
+        allowAutomaticRefresh: false,
+        pushNotifications: false,
+        pageRefreshAfter: 30,
+        cfgTxt: ""
+    }, function (items) {
+  
+        $("textarea[name='cfgTxt']").val(items.cfgTxt);
+        if(items.allowIndividualRetry){
+            $('#pageRefreshAfterId').show();
+        }
+        $('#pageRefreshAfter').val(items.pageRefreshAfter);
+
+        if(items.allowIndividualRetry){
+            $('#allowIndividualRetry-0').prop('checked',true);
+        }else{
+           $('#allowIndividualRetry-1').prop('checked',true); 
+        }
+
+        if(items.allowAutomaticRefresh){
+            $('#allowAutomaticRefresh-0').prop('checked',true);
+        }else{
+           $('#allowAutomaticRefresh-1').prop('checked',true); 
+        }
+
+        if(items.pushNotifications){
+            $('#pushNotifications-0').prop('checked',true);
+        }else{
+           $('#pushNotifications-1').prop('checked',true); 
+        }
+
+
     });
-}
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
-    save_options);
+
+    // when allow automatic refresh is enabled
+    $('input:radio[name="allowAutomaticRefresh"]').change(function () {
+        if ($(this).val() == 'Yes') {
+            $('#pageRefreshAfterId').show();
+        } else {
+            $('#pageRefreshAfterId').hide();
+        }
+    });
+    // when saved
+    $('#saveBtn').click(function () {
+        var allowIndividualRetry = ($("input[name=allowIndividualRetry]:checked").val() === 'Yes');
+        var allowAutomaticRefresh = ($("input[name=allowAutomaticRefresh]:checked").val() === 'Yes');
+        var pushNotifications = ($("input[name=pushNotifications]:checked").val() === 'Yes');
+        var pageRefreshAfter = $("input[name=pageRefreshAfter]").val();
+        var cfgTxt = $("textarea[name=cfgTxt]").val();
+
+       
+        chrome.storage.sync.set({
+            allowIndividualRetry: allowIndividualRetry,
+            allowAutomaticRefresh: allowAutomaticRefresh,
+            pushNotifications: pushNotifications,
+            pageRefreshAfter: pageRefreshAfter,
+            cfgTxt: cfgTxt
+        }, function () {
+            // Update status to let user know options were saved.
+            var status = document.getElementById('status');
+            status.textContent = 'Options saved.';
+            setTimeout(function () {
+                status.textContent = '';
+            }, 750);
+        });
+
+    });
+
+});
