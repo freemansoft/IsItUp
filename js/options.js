cfdac8a515959221a1deb35bf41c5afcc36c6892
Naveen Kumar Gurram <gurram.naveen@gmail.com>
2016-11-27 12:44:06 -0500

    file upload

--- js/options.js
+++ js/options.js
@@ -1,28 +1,32 @@
 $(document).ready(function () {
 
+    document.getElementById('confFileUpld').addEventListener('change', readSingleFile, false);
+
     var filePath;
 
-   /* $("input:file").change(function () {
-        filePath = $(this).val();
-        filePath = filePath.replace("C:\\fakepath\\", "");
-        if(filePath){
-            jQuery.get(filePath, function (data) {
-                var IS_JSON = true;
-                try {
-                    var json = $.parseJSON(data);
-                }
-                catch (err) {
-                    IS_JSON = false;
-                }
-                if(IS_JSON){
-                    $("textarea[name='cfgTxt']").val(data);
-                }else{
-                    alert("Invalid configuration Json file!!");
-                }
-                
-            });
-        }  
-    });*/
+    /*$("input:file").change(function () {
+         filePath = $(this).val();
+         
+         filePath = filePath.replace("C:\\fakepath\\", "");
+         if(filePath){
+             jQuery.get(filePath, function (data) {
+                 var IS_JSON = true;
+                 try {
+                     var json = $.parseJSON(data);
+                 }
+                 catch (err) {
+                     IS_JSON = false;
+                 }
+                 if(IS_JSON){
+                     $("textarea[name='cfgTxt']").val(data);
+                 }else{
+                     alert("Invalid configuration Json file!!");
+                 }
+                 
+             });
+         }  
+     }); */
+
 
 
     // get values from localsync
@@ -33,37 +37,37 @@ $(document).ready(function () {
         pageRefreshAfter: 30,
         cfgTxt: ""
     }, function (items) {
-        if(items.cfgTxt){
+        if (items.cfgTxt) {
             $("textarea[name='cfgTxt']").val(items.cfgTxt);
         }
-        else{
+        else {
             jQuery.get("conf/defaultConf.json", function (data) {
                 $("textarea[name='cfgTxt']").val(data);
             });
         }
-        
 
-        if(items.allowIndividualRetry){
+
+        if (items.allowIndividualRetry) {
             $('#pageRefreshAfterId').show();
         }
         $('#pageRefreshAfter').val(items.pageRefreshAfter);
 
-        if(items.allowIndividualRetry){
-            $('#allowIndividualRetry-0').prop('checked',true);
-        }else{
-           $('#allowIndividualRetry-1').prop('checked',true); 
+        if (items.allowIndividualRetry) {
+            $('#allowIndividualRetry-0').prop('checked', true);
+        } else {
+            $('#allowIndividualRetry-1').prop('checked', true);
         }
 
-        if(items.allowAutomaticRefresh){
-            $('#allowAutomaticRefresh-0').prop('checked',true);
-        }else{
-           $('#allowAutomaticRefresh-1').prop('checked',true); 
+        if (items.allowAutomaticRefresh) {
+            $('#allowAutomaticRefresh-0').prop('checked', true);
+        } else {
+            $('#allowAutomaticRefresh-1').prop('checked', true);
         }
 
-        if(items.pushNotifications){
-            $('#pushNotifications-0').prop('checked',true);
-        }else{
-           $('#pushNotifications-1').prop('checked',true); 
+        if (items.pushNotifications) {
+            $('#pushNotifications-0').prop('checked', true);
+        } else {
+            $('#pushNotifications-1').prop('checked', true);
         }
 
 
@@ -84,8 +88,8 @@ $(document).ready(function () {
         var pushNotifications = ($("input[name=pushNotifications]:checked").val() === 'Yes');
         var pageRefreshAfter = $("input[name=pageRefreshAfter]").val();
         var cfgTxt = $("textarea[name=cfgTxt]").val();
-  
-       
+
+
         chrome.storage.local.set({
             allowIndividualRetry: allowIndividualRetry,
             allowAutomaticRefresh: allowAutomaticRefresh,
@@ -99,7 +103,7 @@ $(document).ready(function () {
             setTimeout(function () {
                 status.textContent = '';
             }, 5000);
-            setTimeout(function(){
+            setTimeout(function () {
                 window.close();
             }, 1000)
         });
@@ -107,3 +111,31 @@ $(document).ready(function () {
     });
 
 });
+
+
+function readSingleFile(evt) {
+    //Retrieve the first (and only!) File from the FileList object
+    var f = evt.target.files[0];
+    if (f) {
+        var r = new FileReader();
+        r.onload = function (e) {
+            var contents = e.target.result;
+            var isJson = true;
+            try {
+                var json = $.parseJSON(contents);
+            }
+            catch (err) {
+                isJson = false;
+            }
+            if (isJson) {
+                $("textarea[name='cfgTxt']").val(contents);
+            } else {
+                alert("Invalid configuration Json file!!");
+            }
+        }
+        r.readAsText(f);
+    } else {
+        alert("Failed to load file");
+    }
+}
+
